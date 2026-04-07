97284939560fb8a2461ea8d4fa18f716ca61c417
Benjamin <benjamin.schroeders@icloud.com>
2026-04-07 09:34:55 +0200

    Amélioration en ligne ( Images encore bugéés en ligne)

--- app/Models/comment.php
+++ app/Models/comment.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class comment extends Model
+class Comment extends Model
 {
     protected $table = 'comments';
     protected $fillable = ['content', 'post_id', 'user_id'];
